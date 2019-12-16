466a0f5e13fde5cb203f38515d40d5499dd62ad8
github username <github email address>
2019-12-16 12:26:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    const match = RegExp(`^${name}`, "i");
+    return drivers.filter(d => match.exec(d));
+}
+
+function fuzzyMatch(drivers, name) {
+    return findMatching(drivers, name);
+}
+
+function matchName(drivers, name) {
+    const match = RegExp(`^${name}`, "i");
+    return drivers.filter(d => match.exec(d.name));
+}   
